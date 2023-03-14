58eab208aeaa8c40f6e8b5940fdce1ba76f0a08a
paul kabuu <kongkabuu9@gmail.com>
2023-03-14 21:35:49 +0300

    completed assingmnet

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    let hq = 42;
+    if(block > hq){
+    return block - hq;
+    }else{
+        return hq-block;
+    }
+}
+
+function distanceFromHqInFeet(block){
+    let feet = 264;
+let distance = distanceFromHqInBlocks(block);
+return distance * feet;
+
+}
+
+
+function distanceTravelledInFeet(start, end){
+    let distance = 264;
+    let block = 0;
+    if(start > end){
+         block = start -end;
+    }else{
+        block = end - start;
+    }
+
+    return block * distance;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+
+    if (feet< 400){
+        return 0;
+    }else if(feet> 400 && feet< 2000){
+
+        return (feet-400) * 0.02;
+    }else if (feet > 2000 && feet <= 2500){
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+
+
+  }
